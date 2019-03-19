396742ba58cbddba0f85e112a63ca8d17cb8e599
Nyame-Wolf <mumenyam@gmail>
2019-03-19 15:26:22 +0300

    Renaming all files to python files

---
+++ others/output_once _per_min_for _5_secs.py
@@ -0,0 +1,6 @@
+# Print Hello World once every second for  5 seconds
+ import time
+ for i in range(5):
+     print("Hello World!")
+     time.sleep(1)
+ 
--- others/renaming_f.py
+++ others/renaming_f.py
@@ -2,4 +2,17 @@ import os
 
 os.chdir('/home/nyame-wolf/snippets_solutions/codewars')
 
-print(os.getcwd())
+# print(os.getcwd()) to find out if we are in the correct directory
+fder = '/home/nyame-wolf/snippets_solutions/codewars'
+for f in os.listdir(fder):
+    # print(f)
+    # print(os.path.splitext(f)) # split file to get a tupel of name and extension
+
+    f_name = os.path.join(fder, f)
+    if not os.path.isfile(f_name):
+        continue
+    oldbase = os.path.splitext(f)
+    # print(file_ext)
+    name = f_name.replace('.txt', '.py')
+    # output =
+    print(os.rename(f_name, name))
